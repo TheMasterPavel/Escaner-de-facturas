c86fdc6ec11b6354ed45da090d4d3f366075f254
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 21:31:21 +0000

    no funciona deploy

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,13 +1,13 @@
-'use server';
 import defineNextJsHandler from '@genkit-ai/next';
 import '@/ai/genkit';
 import * as handleMissingInvoiceData from '@/ai/flows/handle-missing-invoice-data';
 import {NextRequest} from 'next/server';
 
+const handler = defineNextJsHandler({
+  flows: [handleMissingInvoiceData],
+});
+
 export async function POST(req: NextRequest) {
-  const handler = defineNextJsHandler({
-    flows: [handleMissingInvoiceData],
-  });
   return handler(req);
 }
 
