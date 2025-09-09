04d3ebe0fb763cd4a89cfcfff7b65e28013a57b4
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 17:48:46 +0000

    ya?

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,1 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/handle-missing-invoice-data.ts';
--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,10 +1,7 @@
 
 import {defineNextJsHandler} from '@genkit-ai/next';
-import {config} from 'dotenv';
 import '@/ai/genkit';
 import '@/ai/flows/handle-missing-invoice-data';
-
-config();
 
 export const maxDuration = 60; // Set max duration to 60 seconds
 
