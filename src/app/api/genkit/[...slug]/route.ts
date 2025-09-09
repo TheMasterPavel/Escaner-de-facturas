48de321ed192444d25d13003d94d212c6e754298
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 17:19:23 +0000

    arregla absolutamente todos los problemas, la preview funciona mal, si m

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,14 +1,11 @@
 
 import {defineNextJsHandler} from '@genkit-ai/next';
 import {config} from 'dotenv';
-import {ai} from '@/ai/genkit';
+import '@/ai/genkit';
+import '@/ai/flows/handle-missing-invoice-data';
 
 config();
 
-import '@/ai/flows/handle-missing-invoice-data';
-
 export const maxDuration = 60; // Set max duration to 60 seconds
 
-export const POST = defineNextJsHandler({
-  ai,
-});
+export const POST = defineNextJsHandler();
