5e95d3f462b6ba9537062d41515bf43a1fca1d9a
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 21:23:35 +0000

    no se hace el deploy

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ echo "Añadiendo todos los cambios..."
 git add -A
 
 echo "Creando un nuevo commit..."
-git commit --allow-empty -m "Deploy"
+git commit --allow-empty -m "Deploy $(date)"
 
-echo "Subiendo los cambios a GitHub..."
-git push
+echo "Subiendo los cambios a GitHub (forzado)..."
+git push --force
 
 echo "¡ÉXITO! Los cambios han sido subidos a GitHub."
 echo "Vercel debería iniciar un nuevo despliegue ahora."
