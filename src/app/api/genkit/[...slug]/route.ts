f2e32c54eb74de3464638476a58f4ad684cc3a01
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 20:28:37 +0000

    sale lo mismo: [21:25:41.171] Running build in Washington, D.C., USA (Ea

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,8 +1,10 @@
 
 import defineNextJsHandler from '@genkit-ai/next';
 import '@/ai/genkit';
-import '@/ai/flows/handle-missing-invoice-data';
+import * as handleMissingInvoiceData from '@/ai/flows/handle-missing-invoice-data';
 
 export const maxDuration = 120; // Set max duration to 2 minutes
 
-export const POST = defineNextJsHandler();
+export const {POST} = defineNextJsHandler({
+  flows: [handleMissingInvoiceData],
+});
