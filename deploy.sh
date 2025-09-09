213838e30a42b9daf05214cae309c4ad2d788f1b
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 21:20:26 +0000

    sigue igual

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-
 set -e
-
-echo "Inicializando el repositorio Git si es necesario..."
-if [ ! -d ".git" ]; then
-  git init
-  git remote add origin https://github.com/TheMasterPavel/Escaner-de-facturas
-  git branch -M main
-fi
 
 echo "Añadiendo todos los cambios..."
 git add -A
 
 echo "Creando un nuevo commit..."
-COMMIT_MESSAGE="Deploy en $(date +'%Y-%m-%d %H:%M:%S')"
-git commit --allow-empty -m "$COMMIT_MESSAGE"
+git commit --allow-empty -m "Deploy"
 
 echo "Subiendo los cambios a GitHub..."
-git push -u origin main --force
+git push
 
 echo "¡ÉXITO! Los cambios han sido subidos a GitHub."
 echo "Vercel debería iniciar un nuevo despliegue ahora."
