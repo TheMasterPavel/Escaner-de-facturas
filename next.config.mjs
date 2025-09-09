f88b8201b17bc7bd63d7fb758da73c46e55df28c
Pavel Makarov <XDpavel@gmail.com>
2025-09-09 17:23:25 +0000

    Error: Body exceeded 1 MB limit. To configure the body size limit for Se

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   serverActions: {
     bodySizeLimit: '50mb',
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
 
 export default nextConfig;
